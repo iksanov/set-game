c81a3481181843d8a972bea5c0432f99360306a0
iksanov <iksanov@yahoo.com>
2019-01-25 12:22:08 +0300

    add PlaygroundView

---
+++ SetGame/PlaygroundView.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundView.swift
+//  SetGame
+//
+//  Created by MacBookPro on 25/01/2019.
+//  Copyright © 2019 Iksanov. All rights reserved.
+//
+
+import UIKit
+
+class PlaygroundView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
