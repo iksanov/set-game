4124e4c59b0a9d5e75697f446027286d3f5f1d4c
iksanov <iksanov@yahoo.com>
2019-01-28 14:24:23 +0300

    set redraw and layout all of the views small improvements

--- SetGame/PlaygroundView.swift
+++ SetGame/PlaygroundView.swift
@@ -8,10 +8,72 @@
 
 import UIKit
 
+@IBDesignable
 class PlaygroundView: UIView {
-
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // TODO: try to remove this function call
+    // it can be removed if "Content mode" in storyboard is set to "Redraw"
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setNeedsDisplay()
+        setNeedsLayout()
     }
+    
+    @IBInspectable
+    var numberOfCardsOnTheTable = 12
+    
+    lazy var grid = Grid(layout: .aspectRatio(SizeRatio.cardAspectRatio), frame: bounds)
+    
+    override func draw(_ rect: CGRect) {
+        // TODO: try to remove this line
+        super.setNeedsDisplay()
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 50, startAngle: CGFloat.pi, endAngle: 1/2*CGFloat.pi, clockwise: true)
+        path.stroke()
+        
+        // TODO: set background color in initializer
+        // (don't know how)
+        for ind in subviews.indices {
+            subviews[ind].backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // TODO: No need to delete all subviews everytime
+        // it's enough to replace when the number of them didn't changed
+        // even if it did their grid layout can remain the same
+        while !subviews.isEmpty {
+            subviews.first?.removeFromSuperview()
+        }
+        
+        grid.frame = bounds
+        grid.cellCount = numberOfCardsOnTheTable
+        
+        for i in 0..<numberOfCardsOnTheTable {
+            if let newCardFrame = grid[i] {
+                let newCard = SetCardView()
+                let shortenFrame = newCardFrame.zoom(by: SizeRatio.cardWithInsetZoomedBy)
+                newCard.frame = shortenFrame
+                addSubview(newCard)
+            }
+        }
+    }
+}
 
+extension CGRect {
+    func inset(by size: CGSize) -> CGRect {
+        return insetBy(dx: size.width, dy: size.height)
+    }
+    func zoom(by scale: CGFloat) -> CGRect {
+        let newHeight = height * scale
+        let newWidth = width * scale
+        return insetBy(dx: (width - newWidth) / 2, dy: (height - newHeight) / 2)
+    }
+}
+
+extension PlaygroundView {
+    private struct SizeRatio {
+        static let cardAspectRatio: CGFloat = 0.47
+        static let cardWithInsetZoomedBy: CGFloat = 0.97
+    }
 }
--- SetGame/SetCardView.swift
+++ SetGame/SetCardView.swift
@@ -9,20 +9,18 @@
 import UIKit
 
 class SetCardView: UIView {
-
-
     override func draw(_ rect: CGRect) {
+        // TODO: try to remove this line
+        super.setNeedsDisplay()
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
-//        roundedRect.addClip()
         #colorLiteral(red: 0.9764705882, green: 0.8392156863, blue: 0.6862745098, alpha: 1).setFill()
         roundedRect.fill()
     }
-
 }
 
 extension SetCardView {
     private struct SizeRatio {
-        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.03
     }
     private var cornerRadius: CGFloat {
         return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
